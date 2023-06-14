18fac2f420c55bfff9d2bfae5b45c8bf9d142319
Lahcen-KH <lahcenkhaddaoui@hotmail.fr>
2023-06-14 19:26:11 +0100

    vercel

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import '@rainbow-me/rainbowkit/styles.css'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/index.css';
 import Web3Wrapper from '@/components/Layout/Web3Wrapper'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // suppress useLayoutEffect warnings when running outside a browser
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider themes={['light', 'dark', 'cupcake', 'lofi', 'cyberpunk']}>
       <Web3Wrapper>
         <Component {...pageProps} />
+        <Analytics />
       </Web3Wrapper>
     </ThemeProvider>
   )
