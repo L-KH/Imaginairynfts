947469965b894f4fb3851b237303ecc550a84424
Lahcen-KH <lahcenkhaddaoui@hotmail.fr>
2023-06-14 14:23:11 +0100

    googleanalytics

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -3,7 +3,23 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
-      <Head>{}</Head>
+      <Head>{/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J7C8GJVXSQ`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-J7C8GJVXSQ');
+          `,
+          }}
+        />
+        </Head>
       <body>
         <Main />
         <NextScript />
